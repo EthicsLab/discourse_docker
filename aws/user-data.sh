ab3dc1f3cb4be56b330c59094c797739c70e98bb
Joshua "jag" Ginsberg <jag@flowtheory.net>
2015-04-13 18:53:35 -0400

    Build docker image and user-data scripts working.

--- aws/user-data.sh
+++ aws/user-data.sh
@@ -13,17 +13,23 @@ echo "deb https://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/do
 curl https://s3-us-west-2.amazonaws.com/ethicslab-support/ddbconfig.py > /usr/local/bin/ddbconfig
 chmod 0755 /usr/local/bin/ddbconfig
 
+DOCKERHUB_USER=gtethicslab
+DOCKERHUB_PASSWORD=\$mgF5rN53BiIw.]J
+DOCKERHUB_EMAIL=ethicslab@georgetown.edu
+DOCKERHUB_VERSION=latest
 APP=discourse
 
 /usr/local/bin/ddbconfig -a $APP > /tmp/env
 
-mkdir /var/docker
-cd /var/docker
-git clone http://github.com/ethicslab/discourse_docker .
+until docker version; do
+   echo 'waiting for docker'
+   sleep 1
+done
 
-cp containers/app.yml.pre containers/app.yml
-sed -r 's/^([^=]+)=(.*)$/ \1: \2/g' /tmp/env >> containers/app.yml
+DOCKER0_IP=$(ifconfig docker0 | egrep -o 'inet addr:([^ ]+)' | cut -d: -f2)
 
-./launcher bootstrap app
-./launcher start app
+# Log into dockerhub
+docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD -e $DOCKERHUB_EMAIL
 
+# Run our app container dockerhub is hosting
+docker run -d --restart=always --env-file /tmp/env -e LANG=en_US.UTF-8 -e RAILS_ENV=production -e UNICORN_WORKERS=3 -e UNICORN_SIDEKIQS=1 -e RUBY_GC_MALLOC_LIMIT=40000000 -e RUBY_HEAP_MIN_SLOTS=800000 -e DISCOURSE_DB_SOCKET="" -e DOCKER_HOST_IP=$DOCKER0_IP -d -v /var/discourse/shared:/shared --name app -t -p 80:80 "$DOCKERHUB_USER/$(echo $APP | sed 's/_/-/g'):$DOCKERHUB_VERSION" /sbin/boot
